15478d185f482984f364ca631b9487eee9759f82
poi <jesuszzvaleta@gmail.com>
2025-03-24 05:15:42 -0600

    progress --visual

--- _libs/lab/target/debug/deps/lab-0ab17ac317444926.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/toolchain/_libs/lab/target/debug/deps/lab-0ab17ac317444926: src/main.rs
-
-/data/data/com.termux/files/home/toolchain/_libs/lab/target/debug/deps/lab-0ab17ac317444926.d: src/main.rs
-
-src/main.rs:
--- _libs/lab/target/debug/lab.d
+++
@@ -1,1 +0,0 @@
-/data/data/com.termux/files/home/toolchain/_libs/lab/target/debug/lab: /data/data/com.termux/files/home/toolchain/_libs/lab/src/main.rs
